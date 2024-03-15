ba8412e92c1fdfccf5c8389af364788c264bc4ed
Rodolpho Porto <rodolpho.porto@gmail.com>
2024-03-15 09:38:58 -0300

    Correções para o id

--- publication/src/main/java/com/example/publication/mapper/PublicationMapper.java
+++ publication/src/main/java/com/example/publication/mapper/PublicationMapper.java
@@ -4,12 +4,15 @@ import com.example.publication.controller.request.PublicationRequest;
 import com.example.publication.domain.Publication;
 import com.example.publication.repository.entity.PublicationEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PublicationMapper {
 
+    @Mapping(source = "id", target = "id")
     PublicationEntity toPublicationEntity(Publication publication);
 
+    @Mapping(source = "id", target = "id")
     Publication toPublication(PublicationEntity publicationEntity);
 
     Publication toPublication(PublicationRequest publicationRequest);
--- publication/src/main/java/com/example/publication/repository/entity/PublicationEntity.java
+++ publication/src/main/java/com/example/publication/repository/entity/PublicationEntity.java
@@ -2,7 +2,6 @@ package com.example.publication.repository.entity;
 
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Data
